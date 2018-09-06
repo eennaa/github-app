5e600f9ad510067952b4463942b7187e81637ea9
GojkoMaric <gojko.maric@yahoo.com>
2018-09-06 21:02:23 +0200

    Zadatak 3

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,25 @@
     <v-ons-page>
       <app-toolbar>
         <template slot="left"> </template>
-        <template slot="left"> </template>        
-              
-      </app-toolbar>        
+        <template slot="right"> </template>    
+      </app-toolbar>
+      <app-search :query.sync="query" :placeholder="placeholder"></app-search>       
     </v-ons-page>
 </template>
 <script>
 import AppToolbar from './components/AppToolbar.vue'
+import AppSearch from './components/AppSearch.vue'
   export default{
     components: {
-      AppToolbar
+      AppToolbar,
+      AppSearch
+    },
+
+    data() {
+      return {
+        query: "",
+        placeholder: "Wnat someting?!"
+      }
     }
   };
 </script>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-ons-search-input :placeholder="placeholder" :value="query" @input="$emit('update:query', $event.target.value)">
+
+  </v-ons-search-input>
+</template>
+
+<script>
+export default {
+  props: {
+    query : {
+      type: String
+    },
+    placeholder: {
+      type: String,
+      default: "You were searching for something?"
+    }
+  },
+
+ 
+  
+}
+</script>
