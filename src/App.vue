d249fc968a5f3a813e4ca70f02b64ccff0df874a
GojkoMaric <gojko.maric@yahoo.com>
2018-09-06 20:31:56 +0200

    Zadatak 2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
     <v-ons-page>
-      <v-ons-toolbar>
-        <div class="center">{{ title }}</div>
-        <div class="right">
-          <v-ons-toolbar-button>
-            <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
-          </v-ons-toolbar-button>
-        </div>
-      </v-ons-toolbar>
-      <div style="text-align: center; padding-top:10px">Hello World!</div>
-      <p style="text-align: center">
-        <v-ons-button @click="alert">Click Me!</v-ons-button>
-      </p>
+      <app-toolbar>
+        <template slot="left"> </template>
+        <template slot="left"> </template>        
+              
+      </app-toolbar>        
     </v-ons-page>
 </template>
 <script>
+import AppToolbar from './components/AppToolbar.vue'
   export default{
-    data() {
-      return {
-        title: 'My app'
-      };
-    },
-    methods: {
-      alert() {
-        this.$ons.notification.alert('This is an Onsen UI alert notification test.');
-      }
+    components: {
+      AppToolbar
     }
   };
 </script>
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-ons-toolbar>
+    <div class="left">
+      <slot name="left">Levo</slot>
+    </div>
+
+    <div class="center">{{ title }}</div>
+
+    <div class="right">
+      <slot name="right">Desno</slot>      
+    </div>
+    
+  </v-ons-toolbar>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "GitHub App"
+    }
+  }
+}
+</script>
